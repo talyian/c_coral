fd279383d4fa1aad29edd913f225b4b196bb724f
Jimmy <talyian@gmail.com>
2018-02-12 19:30:01 -0500

    allow one-sided ifs

--- README.md
+++ README.md
@@ -2,22 +2,23 @@
 
 [![Build Status](https://travis-ci.org/talyian/coral.svg?branch=coral)](https://travis-ci.org/talyian/coral)
 
-A toy LLVM-based language. A Kaleidoscope tutorial project that gained sentience.
+A toy LLVM-based language. 
+A Kaleidoscope tutorial project that gained sentience.
+A Playground to experiment with language features.
 
 ## Getting Started
 
 Easiest way is to build the docker container that has the dependencies (llvm/clang/flex/bison) installed. (`make docker` will build the container and then dump you inside the container in a shell)
 
 ```
-$ make docker
-<builds docker image "coral">
-   <launches docker image>
-[coral]/work$ make test
-   <compiles and runs tests>
-[coral]/work$ make bin/coral
-[coral]/work$ bin/coral jit your_source_file.coral
+$ docker build -t coral -f dockerenv/Dockerfile .
+$ docker run --rm coral bash
+[coral]/work/src$ coral-test
+... Runs tests ...
+[coral]/work/src$ coral-jit tests/cases/simple/hello_world.coral
+Hello, World!
 ```
 
 ## Dependencies
 
-If you'd prefer to build Coral without using docker, make sure you have `make`, `llvm` 5, `clang` 5, `flex`, and `bison` installed.
+If you'd prefer to build Coral without using docker, make sure you have `make`, `llvm` 5, `clang` 5, `flex`, `bison` installed at a minimum.
--- src/analyzers/ReturnInserter.cc
+++ src/analyzers/ReturnInserter.cc
@@ -8,63 +8,68 @@
 namespace coral {
   namespace analyzers {
 
-	class ReturnReplace : public ast::ExprVisitor {
-	public:
-	  ast::BaseExpr * original;
-	  ast::BaseExpr * out = 0;
+    class ReturnReplace : public ast::ExprVisitor {
+    public:
+      ast::BaseExpr * original;
+      ast::BaseExpr * out = 0;
 
-	  ReturnReplace(ast::BaseExpr * expr) {
-		original = expr;
-		out = expr;
-		if (expr)
-		  expr->accept(this);
-	  }
+      ReturnReplace(ast::BaseExpr * expr) {
+        original = expr;
+        out = expr;
+        if (expr)
+          expr->accept(this);
+      }
 
-	  std::string visitorName() { return "Returnify"; }
+      std::string visitorName() { return "Returnify"; }
 
-	  static ast::BaseExpr * replace(ast::BaseExpr * original) {
-		ReturnReplace rr(original);
-		// std::cout << "Replacing Return: "
-		// 		  << ast::ExprNameVisitor::of(rr.original) << " -- "
-		// 		  << ast::ExprNameVisitor::of(rr.out) << std::endl;
-		return rr.out;
-	  }
+      static ast::BaseExpr * replace(ast::BaseExpr * original) {
+        ReturnReplace rr(original);
+        // std::cout << "Replacing Return: "
+        //            << ast::ExprNameVisitor::of(rr.original) << " -- "
+        //            << ast::ExprNameVisitor::of(rr.out) << std::endl;
+        return rr.out;
+      }
 
-	  void visit(ast::Comment * e) { }
-	  void visit(ast::IfExpr * e) {
-		e->ifbody.reset(ReturnReplace::replace(e->ifbody.release()));
-		e->elsebody.reset(ReturnReplace::replace(e->elsebody.release()));
-		out = e;
-	  }
-	  void visit(ast::Block * e) {
+      void visit(ast::Comment * e) { }
+      void visit(ast::IfExpr * e) {
+        if (!e->elsebody) { out = 0; return; }
+        e->ifbody.reset(ReturnReplace::replace(e->ifbody.release()));
+        e->elsebody.reset(ReturnReplace::replace(e->elsebody.release()));
+        out = e;
+      }
+      void visit(ast::Block * e) {
 
-		auto last_item = std::find_if(
-		  e->lines.rbegin(), e->lines.rend(),
-		  [] (std::unique_ptr<ast::BaseExpr> & cur) { return (cur && ast::ExprTypeVisitor::of(cur.get()) != ast::ExprTypeKind::CommentKind); });
+        auto last_item = std::find_if(
+          e->lines.rbegin(), e->lines.rend(),
+          [] (std::unique_ptr<ast::BaseExpr> & cur) { return (cur && ast::ExprTypeVisitor::of(cur.get()) != ast::ExprTypeKind::CommentKind); });
 
-		if (!e->lines.empty() && last_item != e->lines.rend()) {
-		  auto old_ptr = last_item->release();
-		  auto new_ptr = ReturnReplace::replace(old_ptr);
-		  last_item->reset(new_ptr);
-		  if (new_ptr != old_ptr) { return; }
-		  if (ast::ExprTypeVisitor::of(old_ptr) == ast::ExprTypeKind::ReturnKind) return;
-		  if (ast::ExprTypeVisitor::of(old_ptr) == ast::ExprTypeKind::IfExprKind) return;
-		}
-		e->lines.push_back(
-		  std::unique_ptr<ast::BaseExpr>(
-			new ast::Return(
-			  new ast::IntLiteral("0"))));
-		out = e;
-	  }
-	  void visit(ast::Var * e) { out = new ast::Return(e); }
-	  void visit(ast::Call * e) { out = new ast::Return(e); }
-	  void visit(ast::BinOp * e) { out = new ast::Return(e); }
-	  void visit(ast::IntLiteral * e) { out = new ast::Return(e); }
-	  void visit(ast::StringLiteral * e) { out = new ast::Return(e); }
-	  void visit(ast::While * e) { }
-	  void visit(ast::ForExpr * e) { }
-	  void visit(ast::Return * e) { }
-	};
+        if (!e->lines.empty() && last_item != e->lines.rend()) {
+          auto old_ptr = last_item->release();
+          auto new_ptr = ReturnReplace::replace(old_ptr);
+          if (new_ptr) {
+            last_item->reset(new_ptr);
+            if (new_ptr != old_ptr) { return; }
+            if (ast::ExprTypeVisitor::of(old_ptr) == ast::ExprTypeKind::ReturnKind) return;
+            if (ast::ExprTypeVisitor::of(old_ptr) == ast::ExprTypeKind::IfExprKind) return;
+          } else {
+            last_item->reset(old_ptr);
+          }
+        }
+        e->lines.push_back(
+          std::unique_ptr<ast::BaseExpr>(
+            new ast::Return(
+              new ast::IntLiteral("0"))));
+        out = e;
+      }
+      void visit(ast::Var * e) { out = new ast::Return(e); }
+      void visit(ast::Call * e) { out = new ast::Return(e); }
+      void visit(ast::BinOp * e) { out = new ast::Return(e); }
+      void visit(ast::IntLiteral * e) { out = new ast::Return(e); }
+      void visit(ast::StringLiteral * e) { out = new ast::Return(e); }
+      void visit(ast::While * e) { }
+      void visit(ast::ForExpr * e) { }
+      void visit(ast::Return * e) { }
+    };
   }
 }
 
